c653742bcf549f870b185c068ceb8968bdfa1d9b
karyna73 <karina_apa@hotmail.es>
2020-10-24 17:43:18 -0300

    modelos

---
+++ app/Models/Academico.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Academico extends Model
+{
+    protected $table = 'Academicos';
+    use HasFactory;
+}
